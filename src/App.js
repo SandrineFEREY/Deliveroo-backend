a777daa319f8064be690d5b0f7a308016ccc1716
Sandrine FEREY <sandrineferey@air-de-sandrine.lan>
2021-11-08 18:20:11 +0100

    suite

--- src/App.js
+++ src/App.js
@@ -1,9 +1,70 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import logo from "./assets/deliveroo.png";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCertificate } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(faCertificate);
 
 function App() {
-  return (
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://my-deliveroo-project.herokuapp.com/"
+    );
+    // console.log(response.data);
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <img src={logo} alt="" />
+      <div className="block">
+        <h2>{data.restaurant.name}</h2>
+        <p>{data.restaurant.description}</p>
+        <div>
+          {data.categories
+            .map((elem, index) => {
+              return (
+                <div>
+                  <h2>{elem.name}</h2>
+
+                  {elem.meals.map((elem, index) => {
+                    return (
+                      <div className="pavé">
+                        <h3>{elem.title}</h3>
+                        <div>{elem.description}</div>
+                        <div>{elem.price}</div>
+                        {elem.picture && <img src={elem.picture} alt="" />}
+                        {elem.popular === true && (
+                          <div>
+                            <FontAwesomeIcon
+                              color="orange"
+                              icon="certificate"
+                            />{" "}
+                            <p>populate</p>
+                          </div>
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })
+            .slice(0, 6)}
+        </div>
+      </div>
     </div>
   );
 }
