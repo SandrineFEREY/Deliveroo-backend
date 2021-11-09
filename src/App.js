beb820b4c4c1b71b13af72f35561ba728867bfd9
Sandrine FEREY <sandrineferey@air-de-sandrine.lan>
2021-11-09 13:27:33 +0100

    validation panier

--- src/App.js
+++ src/App.js
@@ -13,12 +13,16 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
-    const response = await axios.get(
-      "https://my-deliveroo-project.herokuapp.com/"
-    );
-    // console.log(response.data);
-    setData(response.data);
-    setIsLoading(false);
+    try {
+      const response = await axios.get(
+        "https://my-deliveroo-project.herokuapp.com/"
+      );
+      // console.log(response.data);
+      setData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   useEffect(() => {
@@ -29,40 +33,67 @@ function App() {
     <span>En cours de chargement... </span>
   ) : (
     <div>
-      <img src={logo} alt="" />
-      <div className="block">
-        <h2>{data.restaurant.name}</h2>
-        <p>{data.restaurant.description}</p>
+      <header>
         <div>
+          <img src={logo} alt="" />
+          <div className="baseline">
+            <h2>{data.restaurant.name}</h2>
+            <div className="baseline-container">
+              <p>{data.restaurant.description}</p>
+              <div>
+                <img
+                  className="baseline-img"
+                  src={data.restaurant.picture}
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="block">
+        <div className="container-bis">
           {data.categories
             .map((elem, index) => {
               return (
-                <div>
+                <div key={index}>
                   <h2>{elem.name}</h2>
-
-                  {elem.meals.map((elem, index) => {
-                    return (
-                      <div className="pavé">
-                        <h3>{elem.title}</h3>
-                        <div>{elem.description}</div>
-                        <div>{elem.price}</div>
-                        {elem.picture && <img src={elem.picture} alt="" />}
-                        {elem.popular === true && (
-                          <div>
-                            <FontAwesomeIcon
-                              color="orange"
-                              icon="certificate"
-                            />{" "}
-                            <p>populate</p>
+                  <div className="meals-container">
+                    {elem.meals.map((elem, index) => {
+                      return (
+                        <div key={index} className="meals">
+                          <div className="meals-description">
+                            <h3>{elem.title}</h3>
+                            <p>{elem.description}</p>
+                            <span>{elem.price}</span>
+                            {elem.popular === true && (
+                              <div>
+                                <FontAwesomeIcon
+                                  color="orange"
+                                  icon="certificate"
+                                />
+                                <p>populate</p>
+                              </div>
+                            )}
                           </div>
-                        )}
-                      </div>
-                    );
-                  })}
+
+                          {elem.picture && (
+                            <div>
+                              <img src={elem.picture} alt="" />
+                            </div>
+                          )}
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
               );
             })
             .slice(0, 6)}
+        </div>
+        <div className="panier">
+          <button>Valider mon panier</button>
+          <p>Votre panier est vide</p>
         </div>
       </div>
     </div>
